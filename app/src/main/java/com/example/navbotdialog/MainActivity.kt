9461618c4f00f35864a301f04d967c9a02efe472
Andhika M. Wijaya <okumaru07@gmail.com>
2025-03-08 12:53:51 +0700

    init

---
+++ app/src/main/java/com/example/navbotdialog/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.navbotdialog
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.navbotdialog.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import androidx.core.graphics.drawable.toDrawable
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.nav_bottom_home -> openFragment(HomeFragment())
+                R.id.nav_bottom_shorts -> openFragment(ShortFragment())
+                R.id.nav_bottom_subscriptions -> openFragment(SubscriptionsFragment())
+                R.id.nav_bottom_library -> openFragment(LibraryFragment())
+            }
+            true
+        }
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener{
+            showBottomDialog()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> openFragment(HomeFragment())
+            R.id.nav_settings -> openFragment(SettingsFragment())
+            R.id.nav_share -> openFragment(ShareFragment())
+            R.id.nav_about -> openFragment(AboutFragment())
+            R.id.nav_logout -> Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun openFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun showBottomDialog() {
+        val dialogs = Dialog(this)
+        dialogs.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialogs.setContentView(R.layout.bottomsheetlayout)
+
+        val videoLayout: LinearLayout = dialogs.findViewById(R.id.layoutVideo)
+        val shortsLayout: LinearLayout = dialogs.findViewById(R.id.layoutShorts)
+        val liveLayout: LinearLayout = dialogs.findViewById(R.id.layoutLive)
+
+        videoLayout.setOnClickListener {
+            dialogs.dismiss()
+            openFragment(VideoFragment())
+        }
+
+        shortsLayout.setOnClickListener {
+            dialogs.dismiss()
+            openFragment(ShortFragment())
+        }
+
+        liveLayout.setOnClickListener {
+            dialogs.dismiss()
+            openFragment(LiveFragment())
+        }
+
+        dialogs.show();
+        dialogs.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialogs.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable());
+        dialogs.window?.attributes?.windowAnimations = R.style.DialogAnimation;
+        dialogs.window?.setGravity(Gravity.BOTTOM);
+    }
+}
